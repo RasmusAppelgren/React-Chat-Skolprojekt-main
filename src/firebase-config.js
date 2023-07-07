cbd65c0132643d3b68561f722f315552902c8021
RasmusAppelgren <75739932+RasmusAppelgren@users.noreply.github.com>
2023-07-07 15:49:41 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,7 @@ import { getFirestore } from '@firebase/firestore';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIbgGihqjzqZv4bWmKC0_WLwDiZ-HAFKk",
-    authDomain: "skolprojekt-dd173.firebaseapp.com",
-    projectId: "skolprojekt-dd173",
-    storageBucket: "skolprojekt-dd173.appspot.com",
-    messagingSenderId: "651605026787",
-    appId: "1:651605026787:web:308c96d3d223cd8bc322d6"
+    GÖMT
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
